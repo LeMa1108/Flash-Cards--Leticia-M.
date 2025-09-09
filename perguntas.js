2840dab92455c18d37f73c1939f0c5ee5a83c38a
Leticia Madruga <leticia.madruga@escola.pr.gov.br>
2025-09-09 08:46:44 -0300

    09/09

---
+++ perguntas.js
@@ -0,0 +1,59 @@
+criaCartao{
+    'conhecimentos gerais',
+        'O sushi é um prato original de qual país?',
+        'Japão'
+}
+
+criaCartao{
+    'história',
+        'Qual evento histórico aconteceu entre os anos de 1914 e 1918?',
+        'Primeira Guerra Mundial'
+}
+
+criaCartao{
+    'arte',
+        'Quem pintou Mona Lisa?',
+        'Leonardo da Vinci'
+}
+
+criaCartao{
+    'conhecimentos gerais',
+        ' Em que país foi construído o Muro de Berlim?',
+        'Alemanha'
+}
+
+criaCartao{
+    'categoria',
+        'pergunta',
+        'resposta'
+}
+
+criaCartao{
+    'categoria',
+        'pergunta',
+        'resposta'
+}
+
+criaCartao{
+    'categoria',
+        'pergunta',
+        'resposta'
+}
+
+criaCartao{
+    'categoria',
+        'pergunta',
+        'resposta'
+}
+
+criaCartao{
+    'categoria',
+        'pergunta',
+        'resposta'
+}
+
+criaCartao{
+    'categoria',
+        'pergunta',
+        'resposta'
+}
